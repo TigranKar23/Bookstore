08e1daf93a6ff1ff19e72d4897aa53e0eb3d9a36
tigrankar23 <tigrankar23@gmail.com>
2024-10-25 15:42:55 +0400

    global

--- Bookstore.API/obj/Debug/net8.0/Bookstore.API.AssemblyInfo.cs
+++ Bookstore.API/obj/Debug/net8.0/Bookstore.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookstore.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47eb7c22d0066215f6a255bd3a547073e63a8733")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f8a42454931ab5a38539a068ba2ea1fb04dbb7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookstore.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bookstore.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
