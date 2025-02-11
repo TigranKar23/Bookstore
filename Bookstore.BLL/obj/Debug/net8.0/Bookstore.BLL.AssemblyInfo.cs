c45ae534b76e5268d183d55e1231bfc5cb37b6ec
tigrankar23 <tigrankar23@gmail.com>
2025-02-11 13:01:05 +0400

    Remove generated files from Git tracking

--- Bookstore.BLL/obj/Debug/net8.0/Bookstore.BLL.AssemblyInfo.cs
+++ Bookstore.BLL/obj/Debug/net8.0/Bookstore.BLL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookstore.BLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+710f34ff26cbefe46ee1f3ff888d85a191a3ece0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e04897aa64122e423b4cc0bf1895af090d4c378")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookstore.BLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bookstore.BLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
